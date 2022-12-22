40084404bc4930990623f1051087b2dd06d4e985
Louis Portales <louis.portales@gmail.com>
2022-12-22 11:53:26 +0100

    adding some comments

--- gridpack_on_LLRT3/sub.sh
+++ gridpack_on_LLRT3/sub.sh
@@ -2,10 +2,18 @@
 MassX=$1
 MassY=$2
 
+# if your script requires any specific environment, they should be exported/sourced here
+# e.g.:
+#   export proxy location if need to access data from DPM
+#   source CMSSW
+#   export env variables to get tools on cvmfs
+#   ...       
+
+
 # Go into working directory
 cd /home/llr/cms/portales/hhbbtautau/gridpack_generation/genproductions/bin/MadGraph5_aMCatNLO
 
-# Run script(s)
+# Run script(s) (here the variable PYTHONPATH has to be set explicitely. Usually not the case)
 PYTHONPATH=$PYTHONPATH:/usr/lib64/python2.7/site-packages;\
 ./gridpack_generation.sh NMSSM_XToYH_MX_${MassX}_MY_${MassY} ./NMSSM_XToYH_221202/NMSSM_XToYH_MX_${MassX}_MY_${MassY}
 
